d66f41e47d8a201b2f7fdf03938e4bc0668f4a4d
Artem <artemtrushnikov@mail.ru>
2021-10-31 16:41:44 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string s = "fgjhr5uy547ydrhfdhYfeqfJQ24UHHIKAFSFKHktyktyktyktkcgfndrthrt6uuy(*%^&*$%^345621#!AJokdsjfl;ek;lwerkfglwektgw4ejtoe(*&%^$R%E$^SFHCHGF%R&R";
+        static string ptrc = "0123456789";
+        static string ptrm = "abcdefghijklmnopqrstuvwxyz";
+        static string ptrb = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static string scrap = "~!@#$%^&*()_+|`-=\\[]{};:<>?,./№";
+
+           static string b = string.Empty;
+           static string m = string.Empty;
+           static string c = string.Empty;
+           static string x = string.Empty;
+        static int temp;
+
+        static void Main(string[] args)
+        {
+            
+
+
+            
+
+            for (int z = 0; z < s.Length; z++)
+            {
+                
+                if (Bm(s[z]))
+                {
+                   
+                    m += s[z].ToString();
+                }
+
+                if (Bb(s[z]))
+                {
+                    b += s[z].ToString();
+                }
+
+                if (C(s[z]))
+                {
+                    for (int g = 0;g<c.Length;c++)
+                    {
+                        for (int n = g+1;n < c.Length;n++)
+                        {
+                            if (c[g] > c[n])
+                            {
+                            temp = c[g];
+                            c[g] = c[n];
+                            c[n] = temp;
+                            
+                            }
+                        }
+                    }
+                    c += s[z].ToString();
+                }
+
+                if (X(s[z]))
+                    
+                {
+                    x += s[z].ToString();
+                }
+            }
+
+            
+
+
+            Console.WriteLine(b);
+            Console.WriteLine(m);
+            Console.WriteLine(c);
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+        }
+        static bool Bm(char c)
+        {
+            
+            bool v = false;
+            for (int y = 0; y < ptrm.Length; y++)
+            {
+
+                if (ptrm[y] == c)
+                {
+                    
+                    v = true;
+                    break;
+                }
+
+            }
+
+            return v;
+        }
+
+        static bool Bb(char c)
+        {
+            
+            bool v = false;
+            for (int y = 0; y < ptrb.Length; y++)
+            {
+
+                if (ptrb[y] == c)
+                {
+                    v = true;
+                    break;
+                }
+            }
+
+            return v;
+        }
+
+        static bool C(char c)
+        {
+            
+            bool v = false;
+            for (int y = 0; y < ptrc.Length; y++)
+            {
+                if (ptrc[y] == c)
+                {
+                    v = true;
+                    break;
+                }
+            }
+
+            return v;
+        }
+        static bool X(char c)
+        {
+
+            bool v = false;
+            for (int y = 0; y < scrap.Length; y++)
+            {
+                if (scrap[y] == c)
+                {
+                    v = true;
+                    break;
+                }
+            }
+
+            return v;
+        }
+       
+    
+        
+        }
+    
+    }
+}
---
+++ Сортировка строки/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string random = "frGref32Y4P32I3GrN2yBugvB3irAwxRndUYgbcRa8";
+        static string alpha = "abcdefghijklmnopqrstuvwxyz";
+        static string beta = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static string gamma = "1234567890";
+        static string omega = "№;%:?*!@#$%^&*()_+-=";
+        static string a = string.Empty;
+        static string b = string.Empty;
+        static string g = string.Empty;
+        static string o = string.Empty;
+        
+        static void Main(string[] args)
+        {
+            int[] g11 = new int[1000];
+            string GG = string.Empty;
+            
+            for (int x = 0; x < random.Length; x++)
+            {
+                for (int y = 0; y < alpha.Length; y++)
+                {
+                    if(random[x] == alpha[y])
+                    {
+                        a += random[x];
+                    }
+                }
+            }
+
+            Console.WriteLine("Маленькие буквы: " + Character(a));
+            Console.WriteLine("");
+
+            
+            
+
+            for (int x = 0; x < random.Length; x++)
+            {
+                for (int y = 0; y < beta.Length; y++)
+                {
+                    if (random[x] == beta[y])
+                    {
+                        b += random[x];
+                    }
+                }
+            }
+            Console.WriteLine("Большие буквы: " + CharacterB(b));
+            Console.WriteLine("");
+            
+            for (int x = 0; x < random.Length; x++)
+            {
+                for (int y = 0; y < gamma.Length; y++)
+                {
+                    if (random[x] == gamma[y])
+                    {
+                        g += random[x];
+
+                    }
+                }
+            }
+            int temp;
+            int Col = g.Length;
+            int[] mas = new int[Col];
+            int x5 = 0;
+            while ( x5 < mas.Length)
+            {
+                for (int u = 0; u < g.Length;u++ )
+                {
+                    mas[x5] = int.Parse(g[u].ToString());
+                    x5++;
+                }
+            }
+            for (int i = 0; i < mas.Length; i++)
+            {
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[i] > mas[j])
+                    {
+                        temp = mas[i];
+                        mas[i] = mas[j];
+                        mas[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Цифры: ");
+            for(int x = 0; x < mas.Length;x++)
+            {
+                Console.Write( + mas[x]);
+            }
+            for (int x = 0; x < random.Length; x++)
+            {
+                for (int y = 0; y < omega.Length; y++)
+                {
+                    if (random[x] == omega[y])
+                    {
+                        o += random[x];
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static string Character(string t)
+        {
+            
+        
+            string Sugar = string.Empty;
+            int Index = 0;
+            
+            while (Index < alpha.Length)
+            {
+                for (int net = 0; net < t.Length; net++)
+                {
+
+                    if (t[net] == alpha[Index])
+                    {
+
+                        Sugar += t[net].ToString();
+                        
+                    }
+                }
+                Index++;
+                
+            }
+            return Sugar;
+        }
+        static string CharacterB(string t)
+        {
+            
+
+            string Sugar = string.Empty;
+            int Index = 0;
+
+            while (Index < beta.Length)
+            {
+                for (int net = 0; net < t.Length; net++)
+                {
+
+                    if (t[net] == beta[Index])
+                    {
+
+                        Sugar += t[net].ToString();
+
+                    }
+                }
+                Index++;
+
+            }
+            return Sugar;
+        }
+    }
+}
